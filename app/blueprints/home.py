eb60f8e80691825dacf2dd9ca901fde1ca41f097
aleavy <you@example.com>
2025-02-07 12:55:08 -0300

    Sort by date

--- app/blueprints/home.py
+++ app/blueprints/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, flash, redirect, render_template, current_app, send_from_directory, request
 from flask_login import login_required
 from ..models.posts import Blog_Post
-
+from sqlalchemy import desc
 
 home_page = Blueprint("home", __name__, template_folder="templates")
 
@@ -9,11 +9,11 @@ home_page = Blueprint("home", __name__, template_folder="templates")
 
 @home_page.route("/", methods=['GET'])
 def start():
-    db = current_app.extensions["sqlalchemy"]
     page = request.args.get('page', 1, type=int)  # Get the page number from the URL, default is 1
     per_page = 8  # Number of items per page
 
-    pagination = Blog_Post.query.paginate(page=page, per_page=per_page, error_out=False)    
+    pagination = Blog_Post.query.order_by(desc(Blog_Post.date_created)) \
+                .paginate(page=page, per_page=per_page, error_out=False)
     return render_template("index.html", pagination=pagination)
 
 # It was a function to show files in a folder. 
